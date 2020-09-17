05e6e59c0ca3f20c418e60290d6dc50863333b32
NyxOfDarkness <ibelieve.as@gmail.com>
2020-09-17 16:55:30 -0700

    all js in one folder

--- question1.js
+++ question1.js
@@ -1,8 +1,35 @@
-var userName = prompt('Whats your name prisoner?');
+'use strict'
 
-
-if (userName == 'Rena'){
-    document.write('Welcome Azura ')
-} else {
-    document.write('To the Gallows!' + userName);
+function askUserName(){
+  var userName = prompt("What's your name prisoner?");
+  document.write(userName.toLowerCase());
+  // return userName;
 }
+
+function areYoudovahkin(){
+  var questionAnswer = prompt("Are you the Dovahkin?");
+    document.write("Did you take my sweet roll?!");
+}
+
+function AskTime(){
+  var confirmAnswer = confirm("Press here if you took my sweet roll?");
+  if (confirmAnswer){
+    alert("Give back my sweet roll, you mudcrab!");
+  } else {
+    alert("Had a stent in the theives guild huh?")
+  }
+
+}
+
+
+
+// var userName1 = askUserNameQuestion();
+// var userName2 = askUserNameQuestion();
+// console.log(userName1);
+// console.log(userName2);
+
+// if (userName1 === 'Rena'){
+//     document.write('Dovahkin ' + userName1)
+// } else {
+//     document.write('MudCrab ' + userName1);
+// }
